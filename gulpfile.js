ecf365c8ee63afba4e7d209aada09b5b0539fb67
James Fox <james.fox@datagility.co.uk>
2018-03-28 09:47:18 +0100

    fixes to ci test report generation

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
 
 gulp.task('inlinesource', function () {
-    return gulp.src('./coverage/*.html')
+    return gulp.src('./coverage/**/*.html')
         .pipe(inlinesource({attribute: false}))
         .pipe(gulp.dest('./coverage/inline'));
 });
