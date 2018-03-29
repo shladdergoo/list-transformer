fe9260b53d442074b266215dc52858ad0090c3cb
James Fox <james.fox@datagility.co.uk>
2018-03-29 15:04:17 +0100

    bootstrapping inversify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import 'reflect-metadata';
+import ContainerConfig from './inversify.config';
+
+//TODO: Resolve dependencies.
+
 export * from './greeter';
---
+++ src/inversify.config.ts
@@ -0,0 +1,8 @@
+import { Container } from 'inversify';
+import Types from './types';
+
+let container = new Container();
+
+//TODO: Bindings here.
+
+export default container;
